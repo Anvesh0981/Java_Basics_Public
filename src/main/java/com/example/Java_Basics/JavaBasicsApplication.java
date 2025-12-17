2f08c402c552148efe3fe0f5fa1c5f252e13bde0
Anvesh <anvesh0981@gmail.com>
2025-12-16 20:26:18 -0700

    remove data jpa

--- src/main/java/com/example/Java_Basics/JavaBasicsApplication.java
+++ src/main/java/com/example/Java_Basics/JavaBasicsApplication.java
@@ -2,12 +2,16 @@ package com.example.Java_Basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = { DataSourceAutoConfiguration.class }
+)
 public class JavaBasicsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBasicsApplication.class, args);
+        IO.println("java");
 	}
 
 }
--- src/test/java/com/example/Java_Basics/JavaBasicsApplicationTests.java
+++ src/test/java/com/example/Java_Basics/JavaBasicsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.Java_Basics;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JavaBasicsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.Java_Basics;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JavaBasicsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
